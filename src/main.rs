f5e17fa793cc66932c24de1f4ee244b5f6d59bb7
Sebastian Motraghi <sebm@users.noreply.github.com>
2017-01-16 09:55:19 -0800

    looks slightly better

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,48 @@
 use std::fmt;
-// fn draw_board(board) {
 
-// }
+enum BoardSquare {
+    Empty,
+    ManWhite,
+    ManRed,
+    // KingWhite,
+    // KingRed
+}
+
+impl fmt::Display for BoardSquare {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let x = match *self {
+            BoardSquare::ManWhite => "w",
+            BoardSquare::ManRed => "r",
+            BoardSquare::Empty => " "
+        };
+
+        write!(f, "{}", x)
+    }
+}
+
+fn draw_board(board: [[BoardSquare; 8]; 8]) {
+    print!("╔");
+    for _ in 0..7 {
+        print!("═══╦");
+    }
+    print!("═══╗\n");
+    for row in board.iter() {
+        for square in row.iter() {
+            print!("║ {} ", square);
+        }
+        print!("║\n");
+    }
+    print!("╚");
+    for _ in 0..7 {
+        print!("═══╩");
+    }
+    print!("═══╝\n");
+
+}
 
 fn main() {
-    enum BoardSquare {
-        Empty,
-        ManWhite,
-        ManRed,
-        // KingWhite,
-        // KingRed
-    };
 
-    impl fmt::Display for BoardSquare {
-        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            let x = match *self {
-                BoardSquare::ManWhite => "w",
-                BoardSquare::ManRed => "r",
-                BoardSquare::Empty => " "
-            };
-
-            write!(f, "{}", x)
-        }
-    }
-
-    let board = [
+    let board: [[BoardSquare; 8]; 8] = [
         [ BoardSquare::Empty, BoardSquare::ManRed, BoardSquare::Empty, BoardSquare::ManRed, 
             BoardSquare::Empty, BoardSquare::ManRed, BoardSquare::Empty, BoardSquare::ManRed ],
         [ BoardSquare::ManRed, BoardSquare::Empty, BoardSquare::ManRed, BoardSquare::Empty,
@@ -43,26 +61,6 @@ fn main() {
             BoardSquare::ManWhite, BoardSquare::Empty,BoardSquare::ManWhite, BoardSquare::Empty ],
     ];
 
-
-    // for _ in 0..25 {
-    //     print!("_");
-    // }
-    // print!("\n");
-    for _ in 0..8 {
-        print!("+___");
-    }
-    print!("+\n");
-    for row in board.iter() {
-        print!("|");
-        for square in row.iter() {
-            print!(" {} |", square);
-        }
-        print!("\n");
-        for _ in 0..8 {
-            print!("+___");
-        }
-        print!("+\n");
-
-    }
+    draw_board();
 
 }
